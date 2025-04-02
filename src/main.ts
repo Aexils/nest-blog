42dd2bb992b97376165658e282798c8ce21c9bbb
Alexis <Alexis.levasseur.76@gmail.com>
2025-04-02 12:39:30 -0400

    feat(config): Prêt à dev

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import dotenv from 'dotenv';
+import process from 'node:process';
+
+dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  try {
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    await app.listen(process.env.PORT);
+    console.log(`Server is running on port ${process.env.PORT}`);
+  } catch (error) {
+    console.error('Error starting the server:', error);
+    process.exit(1);
+  }
 }
-bootstrap();
+
+void bootstrap();
